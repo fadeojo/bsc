3b33733342cfb1750305f58714a28143ebf3d200
Olufemi Adeojo <oadeojo@prontoforms.com>
2017-07-23 22:04:05 -0400

    Heroku config

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,16 @@ use Mix.Config
 # which you typically run after static files are built.
 config :bsc, Bsc.Web.Endpoint,
   on_init: {Bsc.Web.Endpoint, :load_from_system_env, []},
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "arcane-badlands-33488.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
+
+# Configure your database
+config :bsc, Bsc.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -61,4 +69,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
--- lib/bsc/web/channels/user_socket.ex
+++ lib/bsc/web/channels/user_socket.ex
@@ -5,8 +5,9 @@ defmodule Bsc.Web.UserSocket do
   channel "room:*", Bsc.Web.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
-  # transport :longpoll, Phoenix.Transports.LongPoll
+  transport :websocket, Phoenix.Transports.WebSocket,
+  timeout:45_000
+  transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
